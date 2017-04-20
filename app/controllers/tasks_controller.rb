c1b4b50f02231d01d423a886003766ad6e81747d
Jeremy Ross <jeremypross@gmail.com>
2017-04-19 20:43:51 -0400

    building out controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
 class TasksController < ApplicationController
+
+  def index
+    tasks = User.find(params[:user_id]).tasks
+    render :json => tasks, status: 200
+  end
+
+  def task_params
+    params
+    .require(:task)
+    .permit(:id, :task, :user_id)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all
+
+    render :json => users, status 200
+  end
+
+  def create
+    user = User.create(user_params)
+  end
+
+  def update
+    user_updated = User.find(params[:id]).update_attributes(user_params)
+    if user_updated
+      head 200
+    else
+      head 400
+    end
+  end
+
+  def destroy
+    user_destroyed = User.find(params[:id]).destroy
+
+    if user_destroyed
+      head 200
+    else
+      head 400
+    end
+  end
+
+  def user_params
+    params
+      .require(:user)
+      .permit(:first_name, :last_name, :email, :password)
+  end
+
 end
