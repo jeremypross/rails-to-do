80694cdeae4c9f7f3498f411df8454e0e7b4262c
Jeremy Ross <jeremypross@gmail.com>
2017-05-02 18:06:31 -0400

    added sessions controller for user logins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # LOGIN ROUTES
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # Users has one to many relationship with Tasks
   resources :users do
     resources :tasks
   end
